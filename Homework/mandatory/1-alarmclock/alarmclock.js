e7c395ce38e95360ce00323016142876186c1c53
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-12 14:07:08 +0200

    alarm-clock-color

--- Homework/mandatory/1-alarmclock/alarmclock.js
+++ Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,46 +1,41 @@
 function setAlarm() {
-  let count = document.querySelector('#timeRemaining');
- let value = document.querySelector('#alarmSet').value
- count.innerText = "Time Remaining:" + '00:' + value;
- let background = document.querySelector('body')
-  
- 
-       const countDown = setInterval(() => {
-         console.log("insert time out", value);
-         value-- 
-         count.innerText = "Time Remaining:" + '00:' + value;
-         if(count.innerText === "Time Remaining:" + '00:' + '0') {
-           clearInterval(countDown)
-           playAlarm()
-           background.style.backgroundColor = '#81ADF1'
-         }
-               }, 1000);
-     
-   
-   
- }
+    let count = document.querySelector("#timeRemaining");
+    let value = document.querySelector("#alarmSet").value;
+    count.innerText = "Time Remaining:" + "00:" + value;
+    let background = document.querySelector("body");
 
+    const countDown = setInterval(() => {
+        console.log("insert time out", value);
+        value--;
+        count.innerText = "Time Remaining:" + "00:" + value;
+        if (count.innerText === "Time Remaining:" + "00:" + "0") {
+            clearInterval(countDown);
+            playAlarm();
+            background.style.backgroundColor = "#81ADF1";
+        }
+    }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
+    document.getElementById("set").addEventListener("click", () => {
+        setAlarm();
+    });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
+    document.getElementById("stop").addEventListener("click", () => {
+        pauseAlarm();
+    });
 }
 
 function playAlarm() {
-  audio.play();
+    audio.play();
 }
 
 function pauseAlarm() {
-  audio.pause();
+    audio.pause();
 }
 
 window.onload = setup;
