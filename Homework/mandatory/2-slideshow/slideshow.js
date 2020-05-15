c4f5f5367b93beeae5d89be03e8d635ad2e95c5a
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-15 14:41:54 +0200

    part1-done

--- Homework/mandatory/2-slideshow/slideshow.js
+++ Homework/mandatory/2-slideshow/slideshow.js
@@ -1,19 +1,36 @@
 // Write your code here
 
 const myImg = [
-    "https://i.postimg.cc/4drqs1Gh/cielo2.jpg",
+    "https://i.postimg.cc/HxH8KqcJ/download.jpg",
     "https://i.postimg.cc/yxHjLpBn/Thorpe-253x180.jpg",
-    "httpsg://i.postimg.cc/pXPk8SsL/phelps.jp",
+    "https://i.postimg.cc/pLff7fhW/download-1.jpg",
+    "https://i.postimg.cc/HLP94p8X/Federica-Pellegrini2-735x400-620x400.jpg",
 ];
 
+var imgNumb = 0;
+
 function forward() {
-    const selectImg = document.querySelector("#mainimg");
-    const newImg = document.createElement("img");
-    selectImg.appendChild(newImg);
-    const img = document.querySelector("#first");
-    img.remove();
-    newImg.src = myImg[0];
+    imgNumb = imgNumb + 1;
+    if (imgNumb === myImg.length) {
+        imgNumb = 0;
+    }
+    const selectImg = document.querySelector("#first");
+    selectImg.src = myImg[imgNumb];
+}
+
+function back() {
+    imgNumb = imgNumb - 1;
+    if (imgNumb === -1) {
+        imgNumb = myImg.length - 1;
+    }
+    const selectImg = document.querySelector("#first");
+    selectImg.src = myImg[imgNumb];
 }
 
 const buttonFow = document.querySelector("#forward");
 buttonFow.addEventListener("click", forward);
+
+const buttonBack = document.querySelector("#back");
+buttonBack.addEventListener("click", back);
+
+fgfg;
